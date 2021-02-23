1cd43e170e3dae1b10e90b12e90a809cf4110770
Benjamin Poon <benjamintpoon@gmail.com>
2021-02-23 12:48:07 -0700

    wip steps

--- app/models/motel.rb
+++ app/models/motel.rb
@@ -1,4 +1,6 @@
 class Motel < ApplicationRecord
+  after_save :geocode_address, if: ->(obj) { obj.address.present? and obj.address_changed? }
+
   def street_address
     address['street']
   end
@@ -6,4 +8,24 @@ class Motel < ApplicationRecord
   def address_second
     "#{address['city']}, #{address['state']} #{address['zip']}"
   end
+
+  private 
+
+    def geocode_address
+      # 0. while you're working Ben will configure postgres for storage of lat/lng
+      # configure postgis
+      # create migration to add lat/lng column on motels table
+      
+      # 1.  # translate our JSON address field into what the mapbox geocoding sdk needs
+      # (probably a string)
+      
+      # 2. read mapbox geocode sdk docs and figure out how to 
+      # instantiate the geocoder if necessary (with API creds)
+      # make the geocode call, providing our address string from step 1.
+
+      # 3. parse geocode response
+      # places = Mapbox::Geocoder.geocode_forward("Chester, NJ", {:proximity => {:longitude => -74.6968, :latitude => 40.7843}})
+      
+      # 4. store it 
+    end
 end
